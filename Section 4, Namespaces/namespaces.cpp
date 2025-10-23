ccc513cc41a450e62026cf129dd5a0e2ef15278d
BigMinionD <littleminiond@outlook.com>
2025-10-22 19:34:16 -0500

    Day 1 Edit
    - Fixed an error where a variable did not output the intended value
    - Added some comments and an extra line to make it more clear what outputs are being made

--- Section 4, Namespaces/namespaces.cpp
+++ Section 4, Namespaces/namespaces.cpp
@@ -13,6 +13,10 @@ namespace second{
 int main(){
     int x = 0;
 
+    // Using first::x will output the x value from namespace first
+    std::cout << first::x << '\n';
+
+    // Just use x by itself to output main()'s version of x
     std::cout << x << '\n';
 
     return 0;
