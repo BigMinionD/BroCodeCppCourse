8940058e005c61b14aa032aea3bb1ed7570d7337
BigMinionD <littleminiond@outlook.com>
2025-12-01 18:29:37 -0600

    Day 11
    - Progressed in one section
    - Learned about arrays

---
+++ Section 32, Arrays/arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Arrays are data structures that can hold multiple values through an index
+
+int main(){
+    double prices[4];
+
+    prices[0] = 5.00;
+    prices[1] = 7.50;
+    prices[2] = 9.99;
+    prices[3] = 15.00;
+
+    std::cout << prices[0] << '\n';
+    std::cout << prices[1] << '\n';
+    std::cout << prices[2] << '\n';
+    std::cout << prices[3] << '\n';
+
+    return 0;
+}
