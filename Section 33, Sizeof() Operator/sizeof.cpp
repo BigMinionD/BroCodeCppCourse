637d6dabc8fe38e0271fc11e81dd86254e6241ea
BigMinionD <littleminiond@outlook.com>
2025-12-08 13:09:22 -0600

    Day 12
    - Progressed in two sections
    - Learned about the sizeof() operator
    - Learned a method for printing out array values

---
+++ Section 33, Sizeof() Operator/sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// the sizeof() operator determines the size in bytes of specific parts
+
+int main(){
+    double gpa = 2.5;
+
+    std::cout << sizeof(gpa) << " bytes\n";
+
+    return 0;
+}
