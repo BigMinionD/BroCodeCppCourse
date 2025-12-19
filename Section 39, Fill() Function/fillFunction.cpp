e28c0418e05e8ec82c00c82a6c849956af58f6f7
BigMinionD <littleminiond@outlook.com>
2025-12-19 16:33:23 -0600

    Day 15
    - Progressed in three sections
    - Learned how to sort an array
    - Learned how to use the fill() function
    - Learned how to put user data into an array

---
+++ Section 39, Fill() Function/fillFunction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// fill() = Fills a range of elements with a specified value
+// fill(begin, end, value)
+
+int main(){
+    const int size = 99;
+    std::string foods[size];
+
+    fill(foods, foods + (size/3), "pizza");
+    fill(foods + (size/3), foods + (size/3)*2, "hamburger");
+    fill(foods + (size/3)*2, foods + size, "hotdog");
+
+    for(std::string food : foods){
+        std::cout << food << '\n';
+    }
+
+    return 0;
+}
